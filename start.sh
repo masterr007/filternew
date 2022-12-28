90ea7835115d73142aecd798b35b745dfdcf5211
masterr007 <121298090+masterr007@users.noreply.github.com>
2022-12-28 16:43:59 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/masterr07/filternew.git /MdiskSearchBotV2
+cd /filternew
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
