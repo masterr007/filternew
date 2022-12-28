10251e3b964e04efe0cf10968db8c39210ca0e1f
masterr007 <121298090+masterr007@users.noreply.github.com>
2022-12-28 16:46:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ git clone https://github.com/masterr07/filternew.git /MdiskSearchBotV2
 cd /filternew
 pip3 install -r requirements.txt
 echo "Starting Bot...."
-python3 main.py
+gunicorn app:app & python3
