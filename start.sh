fa4d508cca827240fd4d6929b2054d6a39fb236d
masterr007 <121298090+masterr007@users.noreply.github.com>
2022-12-28 16:56:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
-echo "Cloning Repo...."
-git clone https://github.com/masterr07/filternew.git /filternew
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/masterr07/filternew /filternew
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /filternew
+fi
 cd /filternew
-pip3 install -r requirements.txt
+pip3 install -U -r requirements.txt
 echo "Starting Bot...."
-gunicorn app:app & python3
+python3 bot.py
